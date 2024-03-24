03fa144a81dc7c0b16832c0e70744a62087f8586
Viktor Kotsiuban <viktor.kotsiuban@gmail.com>
2024-03-24 08:17:18 -0700

    Add project files.

---
+++ CodeForFun/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CodeForFun.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
